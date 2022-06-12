40169c67b7aa4061a33ee7430dc92d28f498336b
Wu Chengyu <54303040+7086cmd@users.noreply.github.com>
2022-06-12 12:37:52 +0800

    fix(transform)

--- src/plugins/markdown/index.ts
+++ src/plugins/markdown/index.ts
@@ -1,10 +1,16 @@
 export default {
   name: 'md-loader',
-  transform(src: string, id: string) {
+  transform(source: string, id: string) {
     if (/\.(md)$/.test(id)) {
-      console.log('Markdown', src.length, id)
+      console.log('Markdown', source.length, id)
+      function replaceAll(base, src, dest) {
+        while (base.includes(src)) {
+          base.replace(src, dest);
+        }
+        return base;
+      }
       return {
-        code: `export default \`${src.replaceAll('`', '\\`')}\``,
+        code: `export default \`${replaceAll(source, '`', '\\`')}\``,
         map: null,
       }
     }
